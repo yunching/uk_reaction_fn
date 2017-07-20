fa145a341ed975e1ba1ab5d2d48ed949b25abc7d
Yun Ching Lim <yunching.lim@gmail.com>
2017-07-19 23:18:01 -0400

    Added sentiment plots.

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -3,12 +3,34 @@ library(tidyverse)
 library(janeaustenr)
 library(dplyr)
 library(stringr)
+library(pdftools)
+library(magrittr)
 
-original_books <- austen_books() %>%
-  group_by(book) %>%
-  mutate(linenumber = row_number(),
-         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
-                                                 ignore_case = TRUE)))) %>%
-  ungroup()
+data(stop_words)
 
-original_books
+tmp <- pdf_text(file.path(".","data","20170620_mark_carney.pdf"))
+text_df <- data_frame(page = 1:length(tmp), text = tmp)
+text_df %<>%
+  unnest_tokens(word, text)
+
+text_df %<>%
+  anti_join(stop_words)
+text_df %>%
+  count(word, sort = TRUE) 
+
+text_df %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 20) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+mark_carney_sentiment <- 
+text_df %>%
+  inner_join(get_sentiments("bing")) 
+%>%
+  count(book, index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
